afbca5f01a3c1ea8e6d2d55467b479e0badfeaea
An <an.logvinenko@gmail.com>
2023-03-20 21:19:47 +0200

    cloud functions lab - part 1

---
+++ functions/cloud_functions/dealership/get-dealership.js
@@ -0,0 +1,79 @@
+/**
+  *
+  * main() will be run when you invoke this action
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+
+const { CloudantV1 } = require('@ibm-cloud/cloudant');
+const { IamAuthenticator } = require('ibm-cloud-sdk-core');
+
+function main(params) {
+    const current_method=params.__ow_method
+
+    if (current_method.toLowerCase()!='get') return params
+
+    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+    const cloudant = CloudantV1.newInstance({
+      authenticator: authenticator
+    });
+    cloudant.setServiceUrl(params.COUCH_URL);
+    let result ={state:params.state}
+    let entries={}
+    if (params.state) {
+      entries = getMatchingRecords(cloudant,'dealerships',{state:params.state});
+    } else {
+       entries = getAllRecords(cloudant,'dealerships');
+    }
+    return entries;
+}
+
+function getDbs(cloudant) {
+     return new Promise((resolve, reject) => {
+         cloudant.getAllDbs()
+             .then(body => {
+                 resolve({ dbs: body.result });
+             })
+             .catch(err => {
+                  console.log(err);
+                 reject({ err: err });
+             });
+     });
+ }
+
+ /*
+ Sample implementation to get the records in a db based on a selector. If selector is empty, it returns all records. 
+ eg: selector = {state:"Texas"} - Will return all records which has value 'Texas' in the column 'State'
+ */
+ function getMatchingRecords(cloudant,dbname, selector) {
+     return new Promise((resolve, reject) => {
+         cloudant.postFind({db:dbname,selector:selector})
+                 .then((result)=>{
+                   resolve({result:result.result.docs,state:selector.state});
+                 })
+                 .catch(err => {
+                    console.log(err);
+                     reject({ err: err });
+                 });
+          })
+ }
+
+
+ /*
+ Sample implementation to get all the records in a db.
+ */
+ function getAllRecords(cloudant,dbname) {
+     return new Promise((resolve, reject) => {
+         cloudant.postAllDocs({ db: dbname, includeDocs: true, limit: 10 })
+             .then((result)=>{
+               resolve({result:result.result.rows,});
+             })
+             .catch(err => {
+                console.log(err);
+                reject({ err: err });
+             });
+         })
+ }
