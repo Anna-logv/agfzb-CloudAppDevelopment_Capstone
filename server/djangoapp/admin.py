17a779f202565deccf6439d0208df7b36cf3eeff
An <an.logvinenko@gmail.com>
2023-03-21 15:20:59 +0200

    lab 5 - Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,70 @@
 from django.contrib import admin
 # from .models import related models
+from .models import CarMake, CarModel
+from datetime import date
+from django import forms
 
+#widjet to change datefield to year
+class DateSelectorWidget(forms.MultiWidget):
+    def __init__(self, attrs=None):
+        current_year=date.today().strftime("%Y")
+        years = [(year, year) for year in range(1940,int(current_year)+1)]
+        widgets = [
+            #forms.Select(attrs=attrs, choices=days),
+            #forms.Select(attrs=attrs, choices=months),
+            forms.Select(attrs=attrs, choices=years),
+        ]
+        super().__init__(widgets, attrs)
 
+    def decompress(self, value):
+        if isinstance(value, date):
+            return [value.year]
+        elif isinstance(value, str):
+            year = value.split('-')
+            return [year[0]]
+        return [None]
+
+    def value_from_datadict(self, data, files, name):
+        year = super().value_from_datadict(data, files, name)
+        print(year[0])
+        return '{}-01-01'.format(year[0])
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel 
+    extra = 0
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'year':
+            kwargs['widget'] = DateSelectorWidget()
+        return super(CarModelInline,self).formfield_for_dbfield(db_field,**kwargs)
 
-# CarModelAdmin class
+#  class
+# form to attach year widget
+class YearCarModelAdminForm(forms.ModelForm):
+  class Meta:
+    model = CarModel
+    widgets = {
+      'year': DateSelectorWidget(),
+    }
+    fields = '__all__'
+
+class CarModelAdmin(admin.ModelAdmin):
+    form = YearCarModelAdminForm
+    fields = ['carmake', 
+              'name',  
+              'dealer_id',  
+              'type',  
+              'year',  
+              'number_of_seats']
+    list_filter = ['carmake', 'type']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'is_lux']
+    inlines = [CarModelInline]
+    list_filter = ['is_lux']
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
-
+from datetime import datetime
 
 # Create your models here.
 
@@ -10,17 +10,52 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+    name=models.CharField(null=False, max_length=100, default='Car')
+    description=models.CharField(null=True,blank=True,max_length=255)
+    is_lux=models.BooleanField(default=False)
+    def _is_lux_str(self):
+        if self.is_lux:
+            return " (LUX) "
+        else:
+            return ""
+    def __str__(self):
+        return self.name + self._is_lux_str()
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
 # - Name
 # - Dealer id, used to refer a dealer created in cloudant database
 # - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
+#Sedan · Hatchback · MPV/Minivan · SUV · CUV/Crossover · Pickup · Jeep · Coupe
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
-
+class CarModel(models.Model):
+    CAR_TYPE_CHOICES = [
+        ('SEDAN', 'Sedan'),
+        ('HATCHBACK', 'Hatchback'),
+        ('MPV', 'MPV/Minivan'),
+        ('SUV', 'SUV'),
+        ('CUV', 'CUV/Crossover'),
+        ('PICKUP', 'Pickup'),
+        ('JEEP', 'Jeep'),
+        ('COUPE', 'Coupe'),
+        ('OTHER', 'Other'),
+    ]
+    carmake=models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    dealer_id=models.IntegerField(default=0)
+    type=models.CharField(
+        null=False,
+        max_length=20,
+        choices=CAR_TYPE_CHOICES,
+        default='SEDAN'
+    )
+    year=models.DateField(default=datetime.now)
+    number_of_seats=models.PositiveIntegerField(default=5)
+    def __str__(self):
+        return self.name+" ("+self.type+") - "+self.year.strftime('%Y')
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
